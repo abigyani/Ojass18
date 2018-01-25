7030aa21b7829ba3ffe91eebc62c3e46772fd1c4
abigyani <ak47gyani@gmail.com>
2018-01-26 02:04:50 +0530

    Initial commit

---
+++ app/src/main/java/in/nitjsr/ojass/Activities/Walkthrough.java
@@ -0,0 +1,88 @@
+package in.nitjsr.ojass.Activities;
+
+import android.content.Intent;
+import android.os.Build;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import in.nitjsr.ojass.Adapters.IntroAdapter;
+import in.nitjsr.ojass.R;
+import in.nitjsr.ojass.Utils.Utilities;
+import me.relex.circleindicator.CircleIndicator;
+
+public class Walkthrough extends AppCompatActivity implements ViewPager.OnPageChangeListener, View.OnClickListener {
+
+    private ViewPager viewPager;
+    private IntroAdapter introAdapter;
+    private CircleIndicator circleIndicator;
+    private int[] introSlides;
+    private Button btnNext, btnSkip;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_walkthrough);
+
+        Utilities.changeStatusBarColor(this);
+
+        viewPager = findViewById(R.id.viewpager_slider);
+        circleIndicator = findViewById(R.id.circular_indicator);
+        btnNext = findViewById(R.id.btn_next);
+        btnSkip = findViewById(R.id.btn_skip);
+
+        btnNext.setOnClickListener(this);
+        btnSkip.setOnClickListener(this);
+
+        introSlides = new int[]{
+          R.layout.intro_slide_1,
+          R.layout.intro_slide_2,
+          R.layout.intro_slide_3,
+          R.layout.intro_slide_4
+        };
+
+        introAdapter = new IntroAdapter(this, introSlides);
+        viewPager.setAdapter(introAdapter);
+        circleIndicator.setViewPager(viewPager);
+        viewPager.addOnPageChangeListener(this);
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        if (position == introSlides.length - 1){
+            btnNext.setText(getString(R.string.gotit));
+            btnSkip.setVisibility(View.GONE);
+        } else {
+            btnNext.setText(getString(R.string.next));
+            btnSkip.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.btn_next){
+            int currItem = viewPager.getCurrentItem();
+            if (currItem < introSlides.length - 1) viewPager.setCurrentItem(currItem + 1);
+            else startLoginActivity();
+        } else if (view.getId() == R.id.btn_skip){
+            startLoginActivity();
+        }
+    }
+
+    private void startLoginActivity() {
+        startActivity(new Intent(this, Login.class));
+        finish();
+    }
+}
