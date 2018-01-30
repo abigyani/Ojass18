3d00feed55b1d6299fdf95873af0af366e1f6938
Sanay Dev <sanay.nitjsr@gmail.com>
2018-01-30 21:33:06 +0530

    Sanay init commit

---
+++ app/src/main/java/in/nitjsr/ojass/Activities/AboutUs.java
@@ -0,0 +1,64 @@
+package in.nitjsr.ojass.Activities;
+
+import android.content.Intent;
+import android.media.Image;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import in.nitjsr.ojass.R;
+
+public class AboutUs extends AppCompatActivity {
+
+    ImageButton fb;
+    ImageButton insta;
+    ImageButton twitter;
+    ImageButton webpage;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+
+        fb = findViewById(R.id.fb);
+        insta = findViewById(R.id.insta);
+        twitter = findViewById(R.id.twitter);
+        webpage = findViewById(R.id.web);
+
+        fb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/Ojassnitjamshedpur"));
+                startActivity(intent);
+            }
+        });
+
+        insta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/ojass_nitjsr"));
+                startActivity(intent);
+            }
+        });
+
+        twitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://twitter.com/Ojass2k16"));
+                startActivity(intent);
+            }
+        });
+
+        webpage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.ojass.in"));
+                startActivity(intent);
+            }
+        });
+
+    }
+}
