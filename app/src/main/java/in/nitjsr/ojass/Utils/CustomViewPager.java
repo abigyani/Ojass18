abd7ced9eab5620f284ecead31a49eacb48d44a5
abigyani <ak47gyani@gmail.com>
2018-02-03 13:34:10 +0530

    Updated

---
+++ app/src/main/java/in/nitjsr/ojass/Utils/CustomViewPager.java
@@ -0,0 +1,42 @@
+package in.nitjsr.ojass.Utils;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Created by Abhishek on 03-Feb-18.
+ */
+
+public class CustomViewPager extends ViewPager{
+
+    private boolean enabled;
+
+    public CustomViewPager(Context context) {
+        super(context);
+        this.enabled = true;
+    }
+
+    public CustomViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.enabled = true;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return this.enabled && super.onTouchEvent(event);
+
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        return this.enabled && super.onInterceptTouchEvent(event);
+
+    }
+
+    public void setPagingEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+}
