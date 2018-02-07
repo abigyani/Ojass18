5266693a74cf3373df43e8fb013722127355a1f4
SHIVAM SRIVASTAVA <shivamsmarty28@gmail.com>
2018-02-08 03:55:08 +0530

    Adi change

--- app/src/main/java/in/nitjsr/ojass/Activities/Walkthrough.java
+++ app/src/main/java/in/nitjsr/ojass/Activities/Walkthrough.java
@@ -6,6 +6,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 import in.nitjsr.ojass.Adapters.IntroAdapter;
 import in.nitjsr.ojass.R;
@@ -18,7 +21,16 @@ public class Walkthrough extends AppCompatActivity implements ViewPager.OnPageCh
     private IntroAdapter introAdapter;
     private CircleIndicator circleIndicator;
     private int[] introSlides;
+    private int[] introSlidesImageView;
     private Button btnNext, btnSkip;
+
+    public static String sliderUrl[] = {
+            "https://firebasestorage.googleapis.com/v0/b/ojass18-1cb0d.appspot.com/o/WalkthroughImages%2FQR.png?alt=media&token=c5b68989-2ccf-4fda-a749-89e69ef1542e",
+            "https://firebasestorage.googleapis.com/v0/b/ojass18-1cb0d.appspot.com/o/WalkthroughImages%2FACT%20IND.png?alt=media&token=fff0d0f7-f9ff-437c-b1c3-ea3de32a84be",
+            "https://firebasestorage.googleapis.com/v0/b/ojass18-1cb0d.appspot.com/o/WalkthroughImages%2FApp%20Map.png?alt=media&token=92413930-09e8-49d7-96cb-7a4153795a90",
+            "https://firebasestorage.googleapis.com/v0/b/ojass18-1cb0d.appspot.com/o/WalkthroughImages%2FLOC.png?alt=media&token=e8aaddbc-5c87-40fa-b19b-f12e550e608d"
+
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,16 +48,26 @@ public class Walkthrough extends AppCompatActivity implements ViewPager.OnPageCh
         btnSkip.setOnClickListener(this);
 
         introSlides = new int[]{
-          R.layout.intro_slide_1,
-          R.layout.intro_slide_2,
-          R.layout.intro_slide_3,
-          R.layout.intro_slide_4
+                R.layout.intro_slide_1,
+                R.layout.intro_slide_2,
+                R.layout.intro_slide_3,
+                R.layout.intro_slide_4
         };
 
-        introAdapter = new IntroAdapter(this, introSlides);
+        introSlidesImageView = new int[] {
+                R.id.iv_intro_slide_1,
+                R.id.iv_intro_slide_2,
+                R.id.iv_intro_slide_3,
+                R.id.iv_intro_slide_4
+        };
+
+        introAdapter = new IntroAdapter(this, introSlides, introSlidesImageView);
         viewPager.setAdapter(introAdapter);
+        viewPager.setOffscreenPageLimit(3);
         circleIndicator.setViewPager(viewPager);
         viewPager.addOnPageChangeListener(this);
+
+
     }
 
     @Override
